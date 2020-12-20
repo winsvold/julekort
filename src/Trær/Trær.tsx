8b99a51f72eccaef6c459d10102a363ca1239a70
Daniel Winsvold <danielwinsvold@gmail.com>
2020-12-20 10:21:06 +0100

    fikser skoganimasjon

--- src/Trær/Trær.tsx
+++ src/Trær/Trær.tsx
@@ -6,41 +6,45 @@ const Style = styled.div`
   position: relative;
   height: 100vh;
   background: linear-gradient(#111, #232);
-  font-size: 1vw;
   overflow: hidden;
 `;
 
 const slide = keyframes`
-from {
-opacity: 0;
-  transform: translateX(150em);
-}
-5% {
-  opacity: 1;
-}
-95% {
-  opacity: 1;
-}
-to {
+  from {
   opacity: 0;
-  transform: translateX(-150em);
-}
+    transform: translateX(150em);
+  }
+  5% {
+    opacity: 1;
+  }
+  95% {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+    transform: translateX(-150em);
+  }
 `;
 
 const StyledTre = styled(Tre)<{ distance: number; delay: number }>`
+  --size: ${(props) => css`calc(5vw / ${props.distance})`};
+  --distance: ${(props) => props.distance};
+  --brightness: ${(props) => 100 * (props.distance / 100)}%;
   animation: ${slide} ${(props) => props.distance}s linear both infinite;
   animation-delay: -${(props) => props.delay}s;
-  fill: hsl(240, 50%, ${(props) => 100 * (props.distance / 100)}%);
-  font-size: ${(props) => css`calc(5vw / ${props.distance})`};
-  height: 200em;
+  fill: hsl(150, 50%, var(--brightness));
+  height: calc(var(--size) * 200);
   position: absolute;
-  bottom: -30em;
-  filter: blur(${(props) => props.distance / 50}rem);
+  bottom: calc(var(--size) * -15);
+  filter: blur(calc(var(--distance) * 0.02vmin));
 `;
 
 function Trær() {
   return (
     <Style>
+      <StyledTre distance={35} delay={Math.random() * 30} />
+      <StyledTre distance={35} delay={Math.random() * 30} />
+      <StyledTre distance={35} delay={Math.random() * 30} />
       <StyledTre distance={25} delay={Math.random() * 30} />
       <StyledTre distance={16} delay={Math.random() * 30} />
       <StyledTre distance={15} delay={Math.random() * 30} />
