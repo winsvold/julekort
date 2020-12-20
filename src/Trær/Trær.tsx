511b613436ee5a9c6d075c76ab0d7e299197e89f
Daniel Winsvold <danielwinsvold@gmail.com>
2020-12-20 11:31:02 +0100

    flere tre-ikoner

--- src/Trær/Trær.tsx
+++ src/Trær/Trær.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import styled, { css, keyframes } from "styled-components/macro";
 import Tre from "../ikoner/tsx/Tre";
+import { HoytSmaltTre, Tre2, Tre3, Jultre } from "../ikoner/tsx";
+import SvgTre4 from "../ikoner/tsx/Tre4";
 
 const Style = styled.div`
   position: relative;
@@ -26,32 +28,33 @@ const slide = keyframes`
   }
 `;
 
-const StyledTre = styled(Tre)<{ distance: number; delay: number }>`
+const SvgStyle = styled.div<{ distance: number; delay: number }>`
   --size: ${(props) => css`calc(5vw / ${props.distance})`};
   --distance: ${(props) => props.distance};
   --brightness: ${(props) => 100 * (props.distance / 100)}%;
   animation: ${slide} ${(props) => props.distance}s linear both infinite;
   animation-delay: -${(props) => props.delay}s;
-  fill: hsl(150, 50%, var(--brightness));
-  height: calc(var(--size) * 200);
   position: absolute;
   bottom: calc(var(--size) * -15);
+  svg {
+    fill: hsl(150, 50%, var(--brightness));
+    height: calc(var(--size) * 200);
+  }
 `;
 
 function Trær() {
   return (
     <Style>
-      <StyledTre distance={35} delay={Math.random() * 30} />
-      <StyledTre distance={35} delay={Math.random() * 30} />
-      <StyledTre distance={35} delay={Math.random() * 30} />
-      <StyledTre distance={25} delay={Math.random() * 30} />
-      <StyledTre distance={16} delay={Math.random() * 30} />
-      <StyledTre distance={15} delay={Math.random() * 30} />
-      <StyledTre distance={14} delay={Math.random() * 30} />
-      <StyledTre distance={9} delay={Math.random() * 30} />
-      <StyledTre distance={7} delay={Math.random() * 30} />
-      <StyledTre distance={4} delay={Math.random() * 30} />
-      <StyledTre distance={2} delay={Math.random() * 30} />
+      <SvgStyle children={<Tre2 />} distance={35} delay={Math.random() * 30} />
+      <SvgStyle children={<Tre />} distance={35} delay={Math.random() * 30} />
+      <SvgStyle children={<Tre3 />} distance={25} delay={Math.random() * 30} />
+      <SvgStyle children={<Tre />} distance={16} delay={Math.random() * 30} />
+      <SvgStyle children={<SvgTre4 />} distance={15} delay={Math.random() * 30} />
+      <SvgStyle children={<Tre />} distance={14} delay={Math.random() * 30} />
+      <SvgStyle children={<Tre />} distance={9} delay={Math.random() * 30} />
+      <SvgStyle children={<HoytSmaltTre />} distance={7} delay={Math.random() * 30} />
+      <SvgStyle children={<Tre />} distance={4} delay={Math.random() * 30} />
+      <SvgStyle children={<Tre />} distance={2} delay={Math.random() * 30} />
     </Style>
   );
 }
--- src/ikoner/tsx/index.tsx
+++ src/ikoner/tsx/index.tsx
@@ -1,2 +1,7 @@
+export { default as HoytSmaltTre } from "./HoytSmaltTre";
+export { default as Jultre } from "./Jultre";
 export { default as Maleri } from "./Maleri";
 export { default as Tre } from "./Tre";
+export { default as Tre2 } from "./Tre2";
+export { default as Tre3 } from "./Tre3";
+export { default as Tre4 } from "./Tre4";
