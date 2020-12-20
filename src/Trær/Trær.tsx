fee6a101d34fde80c077d876c0c62b7665694937
Daniel Winsvold <danielwinsvold@gmail.com>
2020-12-20 01:12:50 +0100

    legger til script for å automatisk optimalisere svg'er og transformere de til tsx-komponenter

---
+++ src/Trær/Trær.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import styled from "styled-components/macro";
+
+const Style = styled.div`
+    
+`;
+
+function Trær() {
+  return (
+    <Style>
+      <Tre
+      </Style>
+  );
+}
+
+export default Trær;
