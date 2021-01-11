a5e3666ac8f21941e1dc92eb2bb9ee52f8e61a1c
Daniel Winsvold <danielwinsvold@gmail.com>
2021-01-11 08:27:01 +0100

    gjør mask mer responsiv

--- pages/mask.tsx
+++ pages/mask.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
-import { SVGProps, useReducer } from "react";
+import { MutableRefObject, SVGProps, useReducer, useRef, useState } from "react";
 import styled from "styled-components/macro";
+import { useMount } from "react-use";
 
 const StyledSvg = styled.svg`
-  border: dashed gray 0.2rem;
   position: absolute;
   top: 0;
   left: 0;
-  z-index: 1000;
+  width: 100%;
+  height: 100%;
 `;
 
 const Circle = (props: SVGProps<SVGCircleElement>) => <circle {...props} filter="url(#blur)" />;
@@ -21,52 +22,72 @@ interface CircleI {
 function reducer(state: CircleI[], action: { type: "newCircle"; x: number; y: number }): CircleI[] {
   switch (action.type) {
     case "newCircle":
-      return [...state, { cx: action.x, cy: action.y, r: 20 }];
+      return [...state, { cx: action.x, cy: action.y, r: 150 }];
     default:
       return state;
   }
 }
 
-export const SVGMask = () => {
+function getRelativePostion(e: React.MouseEvent) {
+  const rect = e.currentTarget.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  return { x, y };
+}
+
+export const SVGMask = (props: { parentRef?: MutableRefObject<HTMLElement> }) => {
   const [circles, dispatch] = useReducer(reducer, []);
+  const [rect, setRect] = useState<ClientRect | undefined>();
+
+  useMount(() => {
+    const rect = props.parentRef?.current?.getBoundingClientRect();
+    setRect(rect);
+  });
+
+  if (!rect) {
+    return null;
+  }
 
   const handleNewCircle = (e: React.MouseEvent) => {
-    const x = (e.nativeEvent.x * 100) / window.innerWidth;
-    const y = (e.nativeEvent.y * 100) / window.innerHeight;
+    const pos = getRelativePostion(e);
     dispatch({
       type: "newCircle",
-      x,
-      y,
+      ...pos,
     });
   };
 
   return (
-    <StyledSvg viewBox="0 0 100 100" onClick={handleNewCircle}>
+    <StyledSvg viewBox={`0 0 ${rect.width} ${rect.height}`} onClick={handleNewCircle}>
       <defs>
         <filter id="blur" x="-40%" y="-40%" height="180%" width="180%">
-          <feGaussianBlur stdDeviation="2.5" />
+          <feGaussianBlur stdDeviation="15" />
         </filter>
       </defs>
       <mask id="mymask">
-        <rect x={0} y={0} height={100} width={100} fill="#eee" />
+        <rect x={0} y={0} height={rect.height} width={rect.width} fill="#eee" />
         {circles.map((circle, i) => (
           <Circle {...circle} fill="black" key={i} />
         ))}
       </mask>
-      <rect x="0" y="0" height={100} width={100} mask="url(#mymask)" fill="black" />
+      <rect x={0} y={0} height={rect.height} width={rect.width} mask="url(#mymask)" fill="black" />
     </StyledSvg>
   );
 };
 
 const Style = styled.div`
+  position: relative;
   background: linear-gradient(#110088, black);
-  min-height: 100vh;
+  height: 80vh;
+  width: 50vw;
+  margin: 10%;
+  margin-top: 60vh;
 `;
 
 function Mask() {
+  const ref = useRef(null);
   return (
-    <Style>
-      <SVGMask />
+    <Style ref={ref}>
+      <SVGMask parentRef={ref} />
     </Style>
   );
 }
